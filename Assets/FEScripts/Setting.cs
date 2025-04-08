54cb2ac9fcf3c5858df56b09efe327c0193486a0
hayatofukuda <manyaminea.dq4@gmail.com>
2025-04-09 08:49:09 +0900

    ✨ 広告処理の追加

--- Assets/FEScripts/Setting.cs
+++ Assets/FEScripts/Setting.cs
@@ -3,9 +3,17 @@ namespace Assets.FEScripts
     public static class Setting
     {
         // 全体で利用する定数
+        // プレイやーの人数の設定
         public const int INIT_PLAYER_COUNT = 3;
         public const int MIN_PLAYER_COUNT = 3;
         public const int MAX_PLAYER_COUNT = 10;
+        // 広告の設定
+        public const string IOS_BANNER_AD_UNIT_ID = "";
+        public const string IOS_INTERSTITIAL_AD_UNIT_ID = "";
+        public const string IOS_REWARD_AD_UNIT_ID = "";
+        public const string ANDROID_BANNER_AD_UNIT_ID = "";
+        public const string ANDROID_INTERSTITIAL_AD_UNIT_ID = "";
+        public const string ANDROID_REWARD_AD_UNIT_ID = "";
 
         // 全体で利用できる変数
         public static int playerCount;
