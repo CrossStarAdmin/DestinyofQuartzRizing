7ebc6720de4e87612edf5cff2f3c2a9295031941
hayatofukuda <manyaminea.dq4@gmail.com>
2025-12-08 09:40:03 +0900

    ✨ 　デザインの最終調整

--- Assets/FEScripts/Scenes/Title/CanvasUI/TitleHeaderCanvasUI.cs
+++ Assets/FEScripts/Scenes/Title/CanvasUI/TitleHeaderCanvasUI.cs
@@ -21,15 +21,15 @@ namespace Assets.FEScripts.Scenes.Title.CanvasUI
             base.InitObject(_canvas);
             // 各コンポーネントを取得
 
-            _purchaseButton = _component.transform.Find("PurchaseButton").GetComponent<OriginButtonComponent>();
-            _playerInfoButton = _component.transform.Find("PlayerInfoButton").GetComponent<OriginButtonComponent>();
+            // _purchaseButton = _component.transform.Find("PurchaseButton").GetComponent<OriginButtonComponent>();
+            // _playerInfoButton = _component.transform.Find("PlayerInfoButton").GetComponent<OriginButtonComponent>();
         }
 
         public override void SetActions(Action[] _actions)
         {
             // IconButtonのアクションをセット
-            _purchaseButton.InitOriginButtonComponent(_actions[0]);
-            _playerInfoButton.InitOriginButtonComponent(_actions[0]);
+            // _purchaseButton.InitOriginButtonComponent(_actions[0]);
+            // _playerInfoButton.InitOriginButtonComponent(_actions[0]);
         }
     }
 }
